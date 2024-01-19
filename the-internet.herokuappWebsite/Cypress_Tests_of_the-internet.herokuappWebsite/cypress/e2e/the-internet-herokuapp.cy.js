8d672c955362cbe94557f083adde32a412d35cd6
Santiag MDO <mdosantiago2@gmail.com>
2024-01-19 23:49:32 +0100

    Corecting tests

--- the-internet.herokuappWebsite/Cypress_Tests_of_the-internet.herokuappWebsite/cypress/e2e/the-internet-herokuapp.cy.js
+++ the-internet.herokuappWebsite/Cypress_Tests_of_the-internet.herokuappWebsite/cypress/e2e/the-internet-herokuapp.cy.js
@@ -7,7 +7,7 @@ describe('the-internet.herokuapp tests', () => {
   it('TC_2AddRemoveElements_001', () => {
 
     // Click on ADD/Remove elements.
-    cy.get('ul > :nth-child(2) > a ').click();
+    cy.get('ul > :nth-child(2) > a ').should('contain', 'Add/Remove Elements').click();
     
     // Click the "Add Element" button.
     cy.get('button').contains('Add Element').click();
@@ -29,15 +29,37 @@ describe('the-internet.herokuapp tests', () => {
   });
   */
   
-  it('TC_4BrokenImages_001', ()  => {
+  it('TC_4BrokenImages_001', () => {
     // Click on Broken images.
-    cy.get('ul > :nth-child(4) > a ').click();
+    cy.get('ul > :nth-child(4) > a ').should('contain', 'Broken Images').click();
 
     // Images are present and displayed on the page.
     cy.get('.example img').should('have.length', 3);
 
   });
   
+  it('TC_5ChallengingDOM_001', () => {
+    // Click on ChallengingDOM.
+    cy.get('ul > :nth-child(5) > a ').should('contain', 'Challenging DOM').click();
+
+    // Seleccionar el boton del div
+    cy.get('a.button').first().should('exist');
+    // Primer Botón
+   // cy.get('a.button').find('.button').should('exist');
+
+    // Último Botón
+    //cy.get('.button button:last').should('do.exit');
+    cy.get('a.button').last().should('exist');
+
+    // Botón del Medio (si hay tres botones)
+    //cy.get('.button button:eq(1)').should('do.exit');  
+    cy.get('a.button').eq(1).should('exist');
+
+
+
+
+
+  });
   
 
 });
