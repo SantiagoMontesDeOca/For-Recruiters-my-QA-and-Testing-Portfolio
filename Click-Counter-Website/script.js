52cc63011eb25aa713fff9dc30e051bbff2ec4f7
Santiag MDO <mdosantiago2@gmail.com>
2024-02-22 11:21:34 +0100

    New website proyect Click Counter

--- Click-Counter-Website/script.js
+++ Click-Counter-Website/script.js
@@ -8,7 +8,7 @@ function incrementCounter() {
 
   if (count % 2 !== 0) {
     document.body.classList.add("escudo_neuquen");
-    document.body.style.backgroundImage = "url('Images/escudo-neuquen.webp')"; // Agregar el escudo de Neuquén como fondo
+    document.body.style.backgroundImage = "url('Images/escudo-neuquen.webp')"; // Agrega el escudo de Neuquén como fondo
   } else {
     document.body.classList.remove("escudo_neuquen");
     var color1 = '#' + Math.floor(Math.random()*16777215).toString(16);
