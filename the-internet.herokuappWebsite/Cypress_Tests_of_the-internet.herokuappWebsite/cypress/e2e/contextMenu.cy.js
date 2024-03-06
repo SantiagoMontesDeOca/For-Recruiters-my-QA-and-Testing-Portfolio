867c3d5ae771ea9495a72ca9cf0ada0d73de8d13
Santiag MDO <mdosantiago2@gmail.com>
2024-03-06 11:28:14 +0100

    re-organizing files

---
+++ the-internet.herokuappWebsite/Cypress_Tests_of_the-internet.herokuappWebsite/cypress/e2e/contextMenu.cy.js
@@ -0,0 +1,22 @@
+describe('Context Menu', () => {
+    beforeEach(() => {
+      cy.visit('https://the-internet.herokuapp.com/');
+      cy.get(':nth-child(21) > a').click();
+      
+    });
+      
+    it('1 login con usuario y contraseña válidos', () => {
+      // Simular un clic derecho en un elemento que generará una alerta
+        cy.get('tu-selector-aqui').rightclick();
+
+        // Esperar a que aparezca la alerta y confirmarla
+        cy.on('window:alert', (alertText) => {
+        expect(alertText).to.equal('Texto de la alerta'); // Ajusta el texto según tu caso
+        // Puedes hacer más aserciones o acciones según sea necesario
+        });
+      
+    });
+           
+  
+  });
+
